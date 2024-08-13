957ec3113a5bf7123a1718ebf01c64f0fc05d4a1
TakhyunKim <kimkih1218@gmail.com>
2024-08-14 00:13:34 +0900

    chore: add conerstone esm build setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      /**
+       * esm 빌드 시 에러가 발생하여 아래 링크 참고하여 해결
+       * https://github.com/cornerstonejs/cornerstone3D/issues/1071#issuecomment-1937372101
+       */
+      "@cornerstonejs/tools": "@cornerstonejs/tools/dist/umd/index.js",
+    },
+  },
+});

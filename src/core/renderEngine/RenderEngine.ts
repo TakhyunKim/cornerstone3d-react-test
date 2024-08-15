b569844ab021fe9093dccec9a6dc41658e09507e
TakhyunKim <kimkih1218@gmail.com>
2024-08-15 15:55:12 +0900

    feat: add cornerstone Render initial setting and RenderEngine class

---
+++ src/core/renderEngine/RenderEngine.ts
@@ -0,0 +1,38 @@
+import {
+  RenderingEngine as CornerstoneRenderingEngine,
+  init as initCore,
+} from "@cornerstonejs/core";
+
+import { initializeCornerstoneTools } from "../tools";
+import { initializeCornerstoneDICOMImageLoader } from "./initializeCornerstoneDICOMImageLoader";
+
+export const RENDERING_ENGINE_ID = "cornerstone_rendering_engine_id";
+
+/**
+ * Using SINGLETON Pattern
+ *
+ * See the documentation linked below for the reasons for using the Singleton pattern.
+ * https://www.cornerstonejs.org/docs/concepts/cornerstone-core/renderingEngine
+ */
+class RenderingEngine {
+  private static renderingEngine: CornerstoneRenderingEngine;
+
+  private static async initialize() {
+    initializeCornerstoneDICOMImageLoader();
+    initializeCornerstoneTools();
+    await initCore();
+
+    RenderingEngine.renderingEngine = new CornerstoneRenderingEngine();
+  }
+
+  public static async getInstance() {
+    if (!RenderingEngine.renderingEngine) {
+      await this.initialize();
+    }
+    return RenderingEngine.renderingEngine;
+  }
+}
+
+const renderingViewerEngine = await RenderingEngine.getInstance();
+
+export { renderingViewerEngine, CornerstoneRenderingEngine };
